d7cc5d9dd53e8fdabdb7f3e330afeadbeed6b85a
zakaahm <60776348+zakaahm@users.noreply.github.com>
2025-07-25 18:48:43 +0200

    adding best practices to tsconfig and vite config files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-  },
-})
+export default defineConfig(({ mode }) => {
+  
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    server: {
+      port: 5173,
+      open: true,       
+      strictPort: true, 
+    },
+    build: {
+      sourcemap: true,
+      outDir: 'dist',
+      emptyOutDir: true,
+      minify: 'esbuild',
+      target: 'esnext',
+    },
+    css: {
+      devSourcemap: true,
+    },
+    esbuild: {
+      drop: env.NODE_ENV === 'production' ? ['console', 'debugger'] : [],
+    },
+  };
+});
